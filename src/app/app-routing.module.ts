489ed8c1e4fb77d963f7ee16cf923f8c1d38bd9a
Geng Sun <gsun@iastate.edu>
2023-05-17 21:43:58 -0500

    Added login and signup page. Re-structured the code
    Added login and signup page with angular routing.  Added header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DndMainComponent } from './dnd-main/dnd-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DndMainComponent },
+  { path: 'dndMain', component: DndMainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
