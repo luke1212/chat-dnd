c1b345ae448f71b958dc73ac30a779783bf9e542
Geng Sun <gsun@iastate.edu>
2023-05-25 23:08:24 -0500

    MongoDB connected to backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const environment = require("./environment"); 
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = environment.mongoDB.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
