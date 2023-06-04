ff12803720f21b702f1453f55c58441e456db61f
Luke Sun <luke890602@gmail.com>
2023-06-04 17:05:25 -0500

    Updated UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from "../services/user.service";
+import { Subscription } from 'rxjs';
+import { User } from '../model/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  private usersSub!: Subscription;
+  users: User[] = [];
 
   constructor(public userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers();
+    this.usersSub = this.userService.getUserUpdateListener().subscribe(
+      (users: User[]) => {
+        this.users = users;
+      }
+    )
+  }
 
   onSubmit(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.userService.addUser(form.value.username, form.value.password);
+    this.userService.addUser("", form.value.username, form.value.password);
     form.resetForm();
+  }
+
+  ngOnDestroy(): void {
+    this.usersSub.unsubscribe();
   }
 }
